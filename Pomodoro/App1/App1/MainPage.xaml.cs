7ae41b627818ff45bb4877c5da70055af8394845
NoodleSushi <ytmikesstuffs@gmail.com>
2023-05-23 19:36:05 +0800

    implement pomodoro functionality

--- Pomodoro/App1/App1/MainPage.xaml.cs
+++ Pomodoro/App1/App1/MainPage.xaml.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using App1.Server;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +13,67 @@ namespace App1
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MainPage : TabbedPage
     {
+        private PomodoroServer Pomodoro = PomodoroServer.GetInstance();
         public MainPage()
         {
             InitializeComponent();
+            Pomodoro.OnRunningChanged += OnPomodoroRunningChanged;
+            Pomodoro.OnModeChanged += OnPomodoroModeChanged;
+            Pomodoro.OnTimeChanged += OnPomodoroTimeChanged;
+
+            Pomodoro.Reset();
+        }
+
+        public void OnPomodoroRunningChanged(bool isRunning)
+        {
+            Device.BeginInvokeOnMainThread(() => {
+                PomSkipButton.IsVisible = isRunning;
+                PomToggleButton.Text = isRunning ? "PAUSE" : "START";
+            });
+        }
+
+        public void OnPomodoroModeChanged(PomodoroServer.Mode mode)
+        {
+
+        }
+
+        public void OnPomodoroTimeChanged(int time)
+        {
+            Device.BeginInvokeOnMainThread(() => {
+                int secs = time % 60;
+                int mins = time / 60;
+                PomTimeLabel.Text = mins.ToString().PadLeft(2, '0') + ":" + secs.ToString().PadLeft(2, '0');
+            });
+        }
+
+        private void OnPomToggleButtonClicked(object sender, EventArgs e)
+        {
+            Pomodoro.Toggle();
+        }
+
+        private void OnPomSkipButtonClicked(object sender, EventArgs e)
+        {
+            Pomodoro.Skip();
+        }
+
+        private void OnPomPomModeButtonClicked(object sender, EventArgs e)
+        {
+            Pomodoro.ChangeMode(PomodoroServer.Mode.Pomodoro);
+        }
+
+        private void OnPomShortModeButtonClicked(object sender, EventArgs e)
+        {
+            Pomodoro.ChangeMode(PomodoroServer.Mode.ShortBreak);
+        }
+
+        private void OnPomLongModeButtonClicked(object sender, EventArgs e)
+        {
+            Pomodoro.ChangeMode(PomodoroServer.Mode.LongBreak);
+        }
+
+        private void OnSignOutButtonClicked(object sender, EventArgs e)
+        {
+            Pomodoro.Reset();
         }
     }
 }
---
+++ Pomodoro/App1/App1/Server/PomodoroServer.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+
+namespace App1.Server
+{
+    public class PomodoroServer
+    {
+        private static PomodoroServer _instance;
+
+        public static PomodoroServer GetInstance()
+        {
+            if (_instance == null)
+                _instance = new PomodoroServer();
+            return _instance;
+        }
+
+        public enum Mode
+        {
+            Pomodoro,
+            ShortBreak,
+            LongBreak,
+        }
+
+        public delegate void ModeChanger(Mode mode);
+        public delegate void IntChanger(int newInt);
+        public delegate void BoolChanger(bool newBool);
+
+        public event BoolChanger OnRunningChanged = delegate { };
+        public event ModeChanger OnModeChanged = delegate { };
+        public event IntChanger OnTimeChanged = delegate { };
+
+        private Timer timer;
+        private Mode CurrentMode = Mode.Pomodoro;
+        private bool IsRunning = false;
+        private int CurrentTime = 0;
+        private int Pomodoros = 0;
+
+        public void Reset()
+        {
+            timer?.Dispose();
+            CurrentMode = Mode.Pomodoro;
+            IsRunning = false;
+            CurrentTime = GetTimeByMode(CurrentMode);
+            Pomodoros = 0;
+
+            OnModeChanged(CurrentMode);
+            OnTimeChanged(CurrentTime);
+            OnRunningChanged(IsRunning);
+        }
+
+        private static int GetTimeByMode(Mode mode)
+        {
+            switch (mode)
+            {
+                case Mode.Pomodoro: return 1500;
+                case Mode.LongBreak: return 900;
+                case Mode.ShortBreak: return 300;
+                default: return 0;
+            }
+        }
+
+        public Mode GetCurrentMode() => CurrentMode;
+        public int GetCurrentTime() => CurrentTime;
+
+        public void ChangeMode(Mode mode)
+        {
+            CurrentMode = mode;
+            CurrentTime = GetTimeByMode(mode);
+            Stop();
+            OnModeChanged(CurrentMode);
+            OnTimeChanged(CurrentTime);
+        }
+
+        public void Toggle()
+        {
+            if (IsRunning)
+                timer.Dispose();
+            else
+                timer = new Timer(_timerElapsed, null, 1000, 1000);
+            IsRunning = !IsRunning;
+            OnRunningChanged(IsRunning);
+        }
+
+        private void Stop()
+        {
+            timer.Dispose();
+            IsRunning = false;
+            OnRunningChanged(IsRunning);
+        }
+
+        public void Skip()
+        {
+            if (!IsRunning)
+                return;
+            UpdateMode();
+        }
+
+        private void UpdateMode()
+        {
+            if (CurrentMode == Mode.Pomodoro)
+            {
+                ChangeMode((Pomodoros == 3) ? Mode.LongBreak : Mode.ShortBreak);
+                Pomodoros = (++Pomodoros) % 4;
+            }
+            else
+                ChangeMode(Mode.Pomodoro);
+        }
+
+        private void _timerElapsed(object state)
+        {
+            if (CurrentTime <= 0)
+                UpdateMode();
+            else
+            {
+                CurrentTime--;
+                OnTimeChanged(CurrentTime);
+            }
+        }
+    }
+}
