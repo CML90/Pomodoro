3c9cce8483d6725c55ecdbccbb7369457adea44b
NoodleSushi <ytmikesstuffs@gmail.com>
2023-05-24 15:57:06 +0800

    Make logo react when tapped, and enable account sign in

--- Pomodoro/App1/App1/SignInPage.xaml.cs
+++ Pomodoro/App1/App1/SignInPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using App1.Server;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -19,20 +20,39 @@ namespace App1
         {
             base.OnAppearing();
 
-            await SignInLogo.TranslateTo(SignInLogo.TranslationX, 15, 200);
+            await SignInLogo.TranslateTo(SignInLogo.TranslationX, 15, 400, Easing.SpringOut);
         }
 
         private async void OnSignInClicked(object sender, EventArgs e)
         {
-            await SignInLogo.TranslateTo(SignInLogo.TranslationX, -350, 120);
+            if (ProfileServer.GetInstance().AccountExists(UsernameEntry.Text, PasswordEntry.Text))
+            {
+                await SignInLogo.TranslateTo(SignInLogo.TranslationX, -350, 400, Easing.SpringIn);
+                await GoToMainPage();
+            }
+        }
+
+        private async Task GoToMainPage()
+        {
             await Navigation.PushAsync(new MainPage());
-            //if (ProfileServer.GetInstance().AccountExists(UsernameEntry.Text, PasswordEntry.Text))
-            //    await Navigation.PushAsync(new MainPage());
         }
 
         private async void OnSignUpClicked(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new SignUpPage());
+        }
+
+        Easing SillyEase = new Easing(x =>
+        {
+            double res = Math.Sin(x * Math.PI);
+            res *= res * (Math.Sin(x * Math.PI * 5.0) + 1.0) * 0.5;
+            return res;
+        });
+
+        private async void OnSignInLogoTapped(object sender, EventArgs e)
+        {
+            await SignInLogo.TranslateTo(SignInLogo.TranslationX, -100, 1000, SillyEase);
+            await SignInLogo.TranslateTo(SignInLogo.TranslationX, 15, 0);
         }
     }
 }
